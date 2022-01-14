6d7472396b2ff0bda2fb8356ee09a387e179292c
Lijian Zhang <Lijian.Zhang@arm.com>
2022-01-14 16:27:34 +0800

    vpp vectors

---
+++ vppinfra/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "--->>> main.exe"
+./main.exe
+echo "--->>> bond.exe"
+./bond.exe
+echo "--->>> buffers.exe"
+./buffers.exe
+echo "--->>> ceq.exe"
+./ceq.exe
+echo "--->>> memcpyle.exe"
+./memcpyle.exe
+echo "--->>> memsetux.exe"
+./memsetux.exe
+echo "--->>> rdma.exe"
+./rdma.exe FFFFFFF
+
